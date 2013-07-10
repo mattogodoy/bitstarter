2bcd53ffe2badbf92cfb6f1c5a9aff1866457b9e
Matto Godoy <mattogodoy@gmail.com>
2013-07-10 09:05:13 +0000

    update web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var index = fs.readFileSync('index.html');
+var buf = new Buffer(index, "utf-8");
 //console.log(index);
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-  response.send(index);
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
